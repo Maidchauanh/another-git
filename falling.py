fc0a72bb97bf8dde1234bc187999af635818fb58
Chau Anh <trungnguyen123akohshit@gmail.com>
2019-05-21 09:32:10 +0700

    create a character which contain the falling object

--- falling.py
+++ falling.py
@@ -1,10 +1,11 @@
 import pygame
 import random
 class fallingOb:
-	def __init__(self,screen,color,y,width,height):	
+	def __init__(self,screen,color,x,y,width,height):	
 		self.screen=screen
 		self.color=color
-		self.x=random.randint(10,900-50)
+		self.x=x
+		#self.x=random.randint(10,900-50)
 		self.y=y
 		self.width=width
 		self.height=height
@@ -12,30 +13,55 @@ class fallingOb:
 		self.y+=10
 		if self.y >=600:
 			self.y=0
-			self.x=random.randint(10,850)
+			self.x=random.randint(10,170)*5
 		pygame.draw.rect(self.screen,self.color,(self.x,self.y,self.width,self.height))
+class Character:
+	def __init__(self,screen,color,x,y,width,height):
+		self.screen=screen 
+		self.color=color
+		self.x=x
+		self.y=y
+		self.width=width
+		self.height=height
+		self.keys=pygame.key.get_pressed()
+	def Draw(self):
+		pygame.draw.rect(self.screen,self.color,(self.x,self.y,self.width,self.height))
+	def Dichuyen(self):
+		global xnhanvat,ynhanvat
+		self.keys=pygame.key.get_pressed()
+		if self.keys[pygame.K_LEFT]:
+			xnhanvat-=10
+		if self.keys[pygame.K_RIGHT]:
+			xnhanvat+=10
 white=(255,255,255)
 black=(0,0,0)
-width,height=40,50
+red=(255,0,0)
+width,height=20,30
 pygame.init()
 screen=pygame.display.set_mode((900,600))
 pygame.display.set_caption("Falling")
 run=True
 Object=[]
 clock=pygame.time.Clock()
-#making the rain drops
+width_nhanvat,Height_nhanvat=50,30
+xnhanvat=900/2-width_nhanvat/2
+ynhanvat=600-Height_nhanvat
 for i in range(1):
-	x=random.randint(10,890)
-	Bia=fallingOb(screen,black,0,width,height)
+	x=random.randint(10,170)*5
+	y=-1*random.randint(300,1000)
+	Bia=fallingOb(screen,black,x,y,width,height)
 	Object.append(Bia)
 #mainloops
 while run :
 	clock.tick(30)
+   	screen.fill(white)
 	for event in pygame.event.get():
 		if event.type==pygame.QUIT:
-			run=False
-   	screen.fill(white)
+			run=False		
 	for o in Object:
 		o.fall()
-    	pygame.display.update()
+	nhanvat=Character(screen,red,xnhanvat,ynhanvat,width_nhanvat,Height_nhanvat)
+	nhanvat.Draw()
+	nhanvat.Dichuyen()
+	pygame.display.update()
 pygame.quit()
